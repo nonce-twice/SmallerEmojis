d8e7bd56d404031574607a83a5aff4085b52ed11
nonce-twice <>
2022-05-09 17:11:51 -0700

    cleanup

--- Main.cs
+++ Main.cs
@@ -10,7 +10,7 @@ using UnityEditor;
 using VRC;
 using System.Collections.Generic;
 
-[assembly:MelonInfo(typeof(SmallerEmojis.SmallerEmojis), "SmallerEmojis", "0.1", "nonce-twice", "https://github.com/nonce-twice/SmallerEmojis")]
+[assembly:MelonInfo(typeof(SmallerEmojis.SmallerEmojis), "SmallerEmojis", "0.1.0", "nonce-twice", "https://github.com/nonce-twice/SmallerEmojis")]
 [assembly:MelonGame("VRChat", "VRChat")]
 
 namespace SmallerEmojis 
@@ -19,49 +19,17 @@ namespace SmallerEmojis
     public class SmallerEmojis : MelonMod
     {
 
-        // Debug for limiting stuff
         public const string Pref_CategoryName = "Smaller Emojis";
-        public bool Pref_DebugOutput = true;
-        public float Pref_EmojiSize = 0.5f;
-
-        private readonly static string[] EmojiList =
-        {
-            "EmojiLike",
-            "EmojiTomato",
-            "Emoji_Fall_Bats"
-        };
+        public bool Pref_DebugOutput = false;
+        public float Pref_EmojiSize = 0.1f;
 
         public override void OnApplicationStart()
         {
             MelonPreferences.CreateCategory(Pref_CategoryName);
-            MelonPreferences.CreateEntry(Pref_CategoryName, nameof(Pref_DebugOutput),   true,  "Show Debug Output");
-            MelonPreferences.CreateEntry(Pref_CategoryName, nameof(Pref_EmojiSize),   0.5f,  "Emoji Size as factor of default");
-            UIExpansionKit.API.ExpansionKitApi.GetExpandedMenu(ExpandedMenu.QuickMenu).AddSimpleButton("HideEmojis", HideEmojis);
+            MelonPreferences.CreateEntry(Pref_CategoryName, nameof(Pref_EmojiSize), 0.1f, "Emoji Max Size [0-1]");
+            MelonPreferences.CreateEntry(Pref_CategoryName, nameof(Pref_DebugOutput),   false,  "Show Debug Output");
+//            UIExpansionKit.API.ExpansionKitApi.GetExpandedMenu(ExpandedMenu.QuickMenu).AddSimpleButton("HideEmojis", HideEmojis);
         }
-
-        private void HideEmojis()
-        {
-            LogDebugMsg($"Setting emoji sizes");
-
-            var particleRenderers = Resources.FindObjectsOfTypeAll<ParticleSystemRenderer>();
-
-            IEnumerable<ParticleSystemRenderer> particleSystems =
-                from renderer in particleRenderers
-                where renderer.gameObject.name.StartsWith("Emoji")
-                select renderer;
-
-            LogDebugMsg($"Found {particleSystems.ToArray().Length.ToString()} possible emoji objects."); 
-
-            foreach(var ps in particleSystems)
-            {
-                if (ps != null)
-                {
-                    LogDebugMsg($"Found {ps.name}");
-                    ps.maxParticleSize = Pref_EmojiSize;
-                }
-            }
-        }
-
         public override void OnSceneWasLoaded(int buildIndex, string sceneName)
         {
             base.OnSceneWasLoaded(buildIndex, sceneName);
@@ -74,33 +42,50 @@ namespace SmallerEmojis
                     break;
             }
         }
-
-
         public override void OnPreferencesSaved()
         {
             ApplyAllSettings();
         }
+
+        private void HideEmojis()
+        {
+            var particleRenderers = Resources.FindObjectsOfTypeAll<ParticleSystemRenderer>();
+
+            IEnumerable<ParticleSystemRenderer> particleSystems =
+                from renderer in particleRenderers
+                where renderer.gameObject.name.StartsWith("Emoji")
+                select renderer;
+
+            LogDebugMsg($"Found {particleSystems.ToArray().Length.ToString()} possible emoji objects."); 
+            LogDebugMsg($"Setting emoji sizes");
+
+            foreach(var ps in particleSystems)
+            {
+                if (ps != null)
+                {
+//                    LogDebugMsg($"Found {ps.name}");
+                    ps.maxParticleSize = Pref_EmojiSize;
+                }
+            }
+        }
+
         private void ApplyAllSettings()
         {
             UpdatePreferences();
+            HideEmojis();
         }
-
-
 
         private void UpdatePreferences()
         {
             Pref_DebugOutput = MelonPreferences.GetEntryValue<bool>(Pref_CategoryName, nameof(Pref_DebugOutput));
-            Pref_EmojiSize   = MelonPreferences.GetEntryValue<float>(Pref_CategoryName, nameof(Pref_EmojiSize));
+            Pref_EmojiSize   = Mathf.Clamp(MelonPreferences.GetEntryValue<float>(Pref_CategoryName, nameof(Pref_EmojiSize)), 0f, 1f);
         }
 
         private void LogDebugMsg(string msg)
         {
             if (!Pref_DebugOutput)
-            {
                 return; 
-            }
             MelonLogger.Msg(msg);
         }
-        
     }
 }
